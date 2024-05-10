ef9d6226cd5df54dcb887c2e591378a0cfd0862e
Qasim Ali <62395180+qasim070@users.noreply.github.com>
2024-05-10 18:15:45 +0500

    created new route

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,43 +1,73 @@
-import { StyleSheet, Image, Pressable } from "react-native";
+import { StyleSheet, Image, TextInput } from "react-native";
 import { Link, Tabs } from "expo-router";
 import { Text, View } from "@/components/Themed";
 const car = require("../../assets/images/ride.png");
 const food = require("../../assets/images/order-food.png");
 
 export default function TabOneScreen() {
+  
+  
   return (
-    <View style={styles.container}>
-      <Text style={{ fontFamily: "Poppins-Bold", fontSize: 20 }}>
+    <>
+    <View style={styles.containerTop}>
+      <Text style={{ fontFamily: "Poppins-Bold", fontSize: 25 }}>
         My Uber App
       </Text>
       <View style={styles.main}>
-        <Link href="/" asChild>
+        <Link href="/book-ride/" asChild>
           <View style={styles.card}>
             <Image source={car} />
-            <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
+            <Text style={{ fontFamily: "Poppins-Medium", fontSize: 18 }}>
               Get A Ride
             </Text>
           </View>
         </Link>
 
-        <Link href="/" asChild>
+        <Link href="/two" asChild>
         <View style={styles.card}>
           <Image source={food} />
-          <Text style={{ fontFamily: "Poppins-Medium", fontSize: 16 }}>
+          <Text style={{ fontFamily: "Poppins-Medium", fontSize: 18 }}>
             Order Food
           </Text>
         </View>
         </Link>
       </View>
     </View>
+
+
+
+
+
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  containerTop: {
+    paddingTop: 40,
+    flex: 0.4,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+  },
+  containerBottom: {
+    borderTopWidth: 5,
+    borderTopColor: '#eee',
+    paddingTop: 10,
+    flex: 0.6,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  innerContainerBtm: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: '#eee',
+    paddingTop: 10,
+    flex: 1,
+    width: '90%',
+    marginBottom: 10,
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
   },
   card: {
     borderRadius: 10,
--- app/book-ride/index.tsx
+++ app/book-ride/index.tsx
@@ -1,52 +1,68 @@
-import { StyleSheet, Image, Pressable } from "react-native";
+import { StyleSheet , TextInput , Pressable } from "react-native";
 import { Link, Tabs } from "expo-router";
+import {FontAwesome , FontAwesome5 , MaterialCommunityIcons} from '@expo/vector-icons/';
 import { Text, View } from "@/components/Themed";
-const car = require("../../assets/images/ride.png");
-const food = require("../../assets/images/order-food.png");
+import { useState } from "react";
 
 export default function BookRide() {
-  return (
-    <View style={styles.container}>
-      <Text style={{ fontFamily: "Poppins-Bold", fontSize: 20 }}>
-        My Uber App
-      </Text>
-      <View style={styles.main}>
-        <Link href="/" asChild>
-          <View style={styles.card}>
-            <Image source={car} />
-            <Text style={{ fontFamily: "Poppins-Bold", fontSize: 16 }}>
-              Get A Ride
-            </Text>
-          </View>
-        </Link>
+  const [destinationVal , setDestiVal] = useState<string | undefined>(undefined);
 
-        <Link href="/" asChild>
-        <View style={styles.card}>
-          <Image source={food} />
-          <Text style={{ fontFamily: "Poppins-Bold", fontSize: 16 }}>
-            Order Food
-          </Text>
+  const getLocName = () => {
+    // alert(destinationVal)
+  }
+
+  return (
+    
+    <View style={styles.containerBottom}>
+     <View style={styles.innerContainerBtm}>
+      <Text style={{ fontFamily: "Poppins-Medium", fontSize: 20 }}>
+          Choose Destination
+        </Text>
+      
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={{height: 40 , borderWidth: 2 , fontFamily: 'Poppins' , borderColor: '#ddd' ,borderRadius: 5, width: '100%', paddingLeft: 60 , fontSize: 16 ,  paddingVertical: 10 ,}}
+            onChangeText={(val: string) => setDestiVal(val)}
+          />
+           <FontAwesome5 name="map-marked-alt" style={{position: 'absolute' , left: 20}} size={20} color="black" />
         </View>
-        </Link>
-      </View>
+        <Pressable onPress={getLocName} style={{ backgroundColor: '#000' , width: '100%' , borderRadius: 10 , marginTop: 10}}>
+          <Text style={{color: '#fff' , fontFamily: 'Poppins-Medium' , fontSize: 16 ,paddingVertical:5 , textAlign: 'center'}}>
+            Search <FontAwesome5 name="search" size={18} color="white" />
+          </Text>
+        </Pressable>
+        <Text className="bg-slate-100 text-6xl">{destinationVal}</Text>
+     </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  inputContainer: {
+    position: 'relative',
+    width: '100%',
     alignItems: "center",
     justifyContent: "center",
   },
-  card: {
-    borderRadius: 10,
+  containerBottom: {
+    paddingTop: 10,
     flex: 1,
-    textAlign: "center",
-    margin: 12,
-    padding: 5,
     alignItems: "center",
-    backgroundColor: "#eee",
+    justifyContent: "flex-start",
+  },
+  innerContainerBtm: {
+    display: 'flex',
+    flexDirection: 'column',
+    backgroundColor: '#eee',
+
+    paddingTop: 10,
+    flex: 1,
+    width: '90%',
+    marginBottom: 10,
+    paddingHorizontal: 20,
+    borderRadius: 15,
+    alignItems: 'center',
+    justifyContent: 'flex-start',
   },
   main: {
     flexDirection: "row",
